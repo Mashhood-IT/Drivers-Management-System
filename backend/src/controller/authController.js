739fa3cd0894953715b5ad7c019d27ec566ae325
Mashhood-IT <mltcontrol10@gmail.com>
2025-05-23 17:31:30 +0500

    updated

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -11,7 +11,10 @@ const loginAdmin = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(401).json({ message: "Invalid password" });
-
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+    
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.JWT_SECRET,
@@ -24,8 +27,8 @@ const loginAdmin = async (req, res) => {
       user: { id: user._id, email: user.email },
     });
   } catch (error) {
-    console.error("Login error:", error);
-    res.status(500).json({ message: "Server error", error });
+    console.error("Login error:", error.message, error.stack);
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 };
 
