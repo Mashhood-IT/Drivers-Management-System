c33fc524d2cfcb8a4de11c5160573f6a355202ae
Mashhood-IT <mltcontrol10@gmail.com>
2025-05-23 17:25:11 +0500

    deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,8 @@
   app.use(express.json());
   app.use(cors({
     origin: process.env.FRONTEND_ORIGIN, 
-    credentials: true 
+    credentials: true ,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }));
   app.use("/api/driver", driverRoutes);
   app.use("/api/auth", authRoutes);
